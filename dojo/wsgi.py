bdaa366c760d456a36ba141dc73cb6006aa0bcb2
Fred Blaise <fred.blaise@protonmail.com>
2021-05-07 15:25:58 +0200

    remove service.instance.id since it is not working right now

--- dojo/wsgi.py
+++ dojo/wsgi.py
@@ -70,7 +70,7 @@ def init_tracing():
         logger.info(f"Service instance ID is {instance_id}")
         resource = Resource.create(attributes={
             "service.name": "DefectDojo",
-            "service.instance.id": instance_id,
+            # "service.instance.id": instance_id,  #  not showing in jaeger and breaks console
             "service.version": __version__
         })
 
