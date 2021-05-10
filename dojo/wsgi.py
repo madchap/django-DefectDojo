1e8a6445b91b3f2e412caf1fccd96cdaa3dd1a71
Yassine Ilmi <9211413+yilmi@users.noreply.github.com>
2021-05-10 13:57:53 +0100

    Swap ptvsd for debugpy, ptvsd deprecated #4115 (#4322)
    * Merging ptvsd into dev profile
    
    * Documentation minor update and script cleanup
    
    * Updating helm charts to remove mentioned of ptvsd
    
    * Updating docker documentation
    
    * Adding comments in code
    
    * pep8 fixes
    
    * Update docker-compose dev to get port from env
    
    * Keeping debug profile, with same entrypoint as dev
    
    * Documentation adjustments
    
    * Setting variable in helmchart to enable debug
    
    * Final docs changes
    
    * Update helm/defectdojo/templates/django-deployment.yaml
    
    Co-authored-by: Damien Carol <damien.carol@gmail.com>
    
    * Update requirements.txt
    
    Co-authored-by: Damien Carol <damien.carol@gmail.com>
    
    * Bump chart version, and rename value
    
    * Rebasing from dev
    
    * pep8 fixes
    
    * Rebasing
    
    Co-authored-by: Damien Carol <damien.carol@gmail.com>

--- dojo/wsgi.py
+++ dojo/wsgi.py
@@ -29,10 +29,6 @@ logger = logging.getLogger(__name__)
 # os.environ["DJANGO_SETTINGS_MODULE"] = "dojo.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dojo.settings.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
 
 # opentelemetry
 from uwsgidecorators import postfork
@@ -83,29 +79,39 @@ def init_tracing():
         logger.info("Telemetry is disabled per DD_ENABLE_TELEMETRY variable.")
 
 
-application = get_wsgi_application()
+# Shouldn't apply to docker-compose dev mode (1 process, 1 thread), but may be needed when enabling debugging in other contexts
+def is_debugger_listening(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return s.connect_ex(('127.0.0.1', port))
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
 
+debugpy_port = os.environ.get("DD_DEBUG_PORT") if os.environ.get("DD_DEBUG_PORT") else 3000
 
-def _check_ptvsd_port_not_in_use(port):
+# Checking for RUN_MAIN for those that want to run the app locally with the python interpreter instead of uwsgi
+if os.environ.get("DD_DEBUG") == "True" and not os.getenv("RUN_MAIN") and is_debugger_listening(debugpy_port) != 0:
+    print("DD_DEBUG is set to True, setting remote debugging on port {}".format(debugpy_port))
+    import traceback
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('127.0.0.1', port))
-    except socket_error as se:
-        return False
+        import debugpy
 
-    return True
-
-
-ptvsd_port = 3000
-if os.environ.get("DD_DEBUG") == "True" and _check_ptvsd_port_not_in_use(ptvsd_port):
-    try:
-        # enable remote debugging
-        import ptvsd
-        ptvsd.enable_attach(address=('0.0.0.0', ptvsd_port))
-        print("ptvsd listening on port " + ptvsd_port)
+        # Required, otherwise debugpy will try to use the uwsgi binary as the python interpreter - https://github.com/microsoft/debugpy/issues/262
+        debugpy.configure({
+                            "python": "python",
+                            "subProcess": True
+                        })
+        debugpy.listen(("0.0.0.0", debugpy_port))
+        print("DebugPy listening on port {}".format(debugpy_port))
+        if os.environ.get("DD_DEBUG_WAIT_FOR_CLIENT") == "True":
+            print("Waiting for the debugging client to connect on port {}".format(debugpy_port))
+            debugpy.wait_for_client()
+            print("Debugging client connected, resuming execution")
     except Exception as e:
-        print("Generic exception caught with DD_DEBUG on. Passing.")
+        print("Exception caught while enabling debug mode, passing. \nPrinting error {}".format(traceback.format_exc()))
+        pass
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
